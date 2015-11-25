b218e11d5dcb93628e97ae1b74cee1aaeb99036e
Arnold <Arnold@Arnold-Mac.local>
2015-11-25 16:42:20 +0200

    adding the 5D experience feature and layout

---
+++ app/src/main/java/com/app/centurycinema/newcenturycinema/Experience5D.java
@@ -0,0 +1,16 @@
+package com.app.centurycinema.newcenturycinema;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by Arnold on 11/25/15.
+ */
+public class Experience5D extends ActionBarActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.experience_5d);
+    }
+}
