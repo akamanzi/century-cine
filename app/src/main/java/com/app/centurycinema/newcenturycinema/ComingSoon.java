02afca7b8930b863c7307ad171617f4abab01d42
Arnold <Arnold@Arnold-Mac.local>
2015-11-25 16:39:01 +0200

    adding the coming soon feature layout and content

---
+++ app/src/main/java/com/app/centurycinema/newcenturycinema/ComingSoon.java
@@ -0,0 +1,17 @@
+package com.app.centurycinema.newcenturycinema;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by Arnold on 11/25/15.
+ */
+public class ComingSoon extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.coming_soon);
+    }
+}
