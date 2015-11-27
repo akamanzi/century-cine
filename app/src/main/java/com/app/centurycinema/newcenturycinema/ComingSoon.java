378e7220ee4703d9f2bfef05b25c6f71ae79def1
Arnold <Arnold@Arnold-Mac.local>
2015-11-27 16:20:56 +0200

    adding link for the trailer upon button click

--- app/src/main/java/com/app/centurycinema/newcenturycinema/ComingSoon.java
+++ app/src/main/java/com/app/centurycinema/newcenturycinema/ComingSoon.java
@@ -1,17 +1,34 @@
 package com.app.centurycinema.newcenturycinema;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 /**
  * Created by Arnold on 11/25/15.
  */
 public class ComingSoon extends ActionBarActivity {
 
+    ImageButton imageButton;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.coming_soon);
+
+        imageButton = (ImageButton) findViewById(R.id.trailer_button);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent youtubeLink = new Intent(Intent.ACTION_VIEW);
+                youtubeLink.setData(Uri.parse("https://www.youtube.com/watch?v=7GqClqvlObY"));
+                startActivity(youtubeLink);
+            }
+        });
     }
 }
